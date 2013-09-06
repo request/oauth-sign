8f38bc43429af8803eb17f487f0559bdaed933ea
Hyungjin Kim <hyungjin.kim@dena.com>
2013-09-06 21:33:52 +0900

    Handle array param values and sort after encode

--- index.js
+++ index.js
@@ -20,10 +20,25 @@ function hmacsign (httpMethod, base_uri, params, consumer_secret, token_secret) 
   // adapted from https://dev.twitter.com/docs/auth/oauth and 
   // https://dev.twitter.com/docs/auth/creating-signature
 
-  var querystring = Object.keys(params).sort().map(function(key){
-    // big WTF here with the escape + encoding but it's what twitter wants
-    return escape(rfc3986(key)) + "%3D" + escape(rfc3986(params[key]))
-  }).join('%26')
+  // encode before sorting.
+  // see http://tools.ietf.org/html/rfc5849#section-3.4.1.3.2
+  // e.g. c2 < c@, but c2 > c%40.
+  var querystring = Object.keys(params).sort(function(a, b){
+    // simplified compare function because keys are unique
+    return rfc3986(a) > rfc3986(b) ? 1 : 0
+  }).reduce(function(map, key){
+    // querystring builds value as an array for same keys
+    // e.g. 'a3=a&c2&a3=2+q' becomes { a3: ['2 q', 'a'], c2: '' }
+    if (Array.isArray(params[key])) {
+      params[key].map(rfc3986).sort().forEach(function(val){
+        map.push(escape(rfc3986(key)) + "%3D" + escape(val))
+      })
+    } else {
+      // big WTF here with the escape + encoding but it's what twitter wants
+      map.push(escape(rfc3986(key)) + "%3D" + escape(rfc3986(params[key])))
+    }
+    return map;
+  }, []).join('%26')
 
   var base = [
     httpMethod ? httpMethod.toUpperCase() : 'GET',
